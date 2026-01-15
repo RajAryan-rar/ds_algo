bad622fdb8e9b507e727efc94d214af194fe6cea
RajAryan-rar <raarra39@gmail.com>
2026-01-15 18:38:47 +0530

    solved edit_distance_problem

---
+++ dp/edit_distance.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> dp;
+int f(string &s1, string &s2, int i, int j) {
+    if(i == 0) return j;
+    if(j == 0) return i;
+
+    if(dp[i][j] != -1) return dp[i][j];
+
+    if(s1[i] == s2[j]) return dp[i][j] = f(s1,s2,i-1,j-1);
+
+    else return dp[i][j] =1 + min(f(s1,s2,i-1,j-1), min(f(s1,s2,i,j-1),f(s1,s2,i-1,j)));
+} 
+
+int f_bu(string &s1, string &s2, int m, int n) {
+    for(int i=0; i<=m; i++) {
+        for(int j=0; j<=n; j++) {
+            if(i == 0) dp[i][j] = j;
+            else if(j == 0) dp[i][j] = i;
+            else if(s1[i] == s2[j]) dp[i][j] = dp[i-1][j-1];
+
+            else {
+                dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+            }
+        }
+    }
+    return dp[m][n];
+}
+
+int minDistance(string word1, string word2) {
+    int m = word1.length();
+    int n = word2.length();
+
+    if(m == 0 && n == 0) return 0;
+
+    dp.clear();
+    dp.resize(505, vector<int>(505,-1));
+
+    string s1 = " ";
+    string s2 = " ";
+    s1 += word1;
+    s2 += word2;
+
+    return f(s1,s2,m,n);
+}
+
+int main() {
+    string word1;
+    cin>>word1;
+    string word2;
+    cin>>word2;
+
+    cout<<minDistance(word1,word2);
+
+    return 0;
+}
--- ptod.cpp
+++ ptod.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
 class Node{
@@ -19,21 +20,52 @@ void display(Node* temp) {
     cout<<endl;
 }
 
-
+int f(int n) {
+        int r = 0;
+        while(n > 0) {
+            int ld = n%10;
+            r *= 10;
+            r += ld;
+            n /= 10;
+        }
+        
+        return r;
+    }
+    bool isSameAfterReversals(int num) {
+        int a = f(num);
+        cout<<a<<endl;
+        int b = f(a);
+        cout<<b<<endl;
+        return (a == b) ? true : false;
+    }
 
 int main() {
-    Node* a = new Node(5);
-    Node* b = new Node(4);
-    Node* c = new Node(3);
-    Node* d = new Node(2);
-    Node* e = new Node(1);
+    // Node* a = new Node(5);
+    // Node* b = new Node(4);
+    // Node* c = new Node(3);
+    // Node* d = new Node(2);
+    // Node* e = new Node(1);
 
-    a->next = b;
-    b->next = c;
-    c->next = d;
-    d->next = e;
+    // a->next = b;
+    // b->next = c;
+    // c->next = d;
+    // d->next = e;
 
-    display(a);
+    // display(a);
+
+    // isSameAfterReversals(526);
+
+    long long ans = 0;
+    int x =  -125;
+    while (x != 0) {
+        ans = ans * 10 + x % 10;
+        if (ans > INT_MAX || ans < INT_MIN)
+            return 0;
+        x /= 10;
+        cout<<"x: "<<x<<", ans: "<<ans<<endl;
+    }
+
+    cout<<ans;
 
     return 0;
 }

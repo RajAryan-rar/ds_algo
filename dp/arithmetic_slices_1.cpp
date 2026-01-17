6be91aba1e9404e9bb87ded3db33618d485ddeca
RajAryan-rar <raarra39@gmail.com>
2026-01-17 09:15:49 +0530

    solved arithmetic_slices_1

---
+++ dp/arithmetic_slices_1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> arr;
+    vector<int> dp;
+    int f(int i) {
+        if(i < 2) return 0;
+        if(arr[i] - arr[i-1] != arr[i-1] - arr[i-2]) return 0;
+        if(dp[i] != INT_MIN) return dp[i];
+        return dp[i] =  1 + f(i-1);
+    }
+    int f_bu() {
+        dp.clear();
+        dp.resize(arr.size(),0);
+        int count = 0;
+        for(int i=2; i<arr.size(); i++) {
+            if(arr[i] - arr[i-1] != arr[i-1] - arr[i-2]) dp[i] = 0;
+            else dp[i] = 1 + dp[i-1];
+            count += dp[i];
+        }
+        return count;
+    }
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        arr = nums;
+        dp.clear();
+        dp.resize(5005, INT_MIN);
+        for(int i=2; i<nums.size(); i++) {
+            int x = f(i);
+        }
+        int count = 0;
+        for(int i=0; i<nums.size(); i++) {
+            if(dp[i] == INT_MIN) continue;
+            else count += dp[i];
+        }
+        return count;
+
+        //return f_bu();
+    }
+};
+
+int main() {
+    
+
+    return 0;
+}

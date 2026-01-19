06af872253f4426e2f9d84ace78f9701ae7dedcd
RajAryan-rar <raarra39@gmail.com>
2026-01-19 23:38:35 +0530

    implemented bottom_up for longest_common_subsequence

--- dp/longest_common_subsequence.cpp
+++ dp/longest_common_subsequence.cpp
@@ -11,14 +11,25 @@ int f(string &s1, string &s2, int i, int j) {
     return dp[i][j] = max(f(s1,s2,i+1,j), f(s1,s2,i,j+1));
 }
 
-int f_bu(string &s1, string &s2, int i, int j) {
-    //to be done
+int f_bu(string &s1, string &s2) {
+    dp.clear();
+    dp.resize(1005, vector<int>(1005,0));
+    for(int i=s1.length()-1; i>=0; i--) {
+        for(int j=s2.length()-1; j>=0; j--) {
+            if(s1[i] == s2[j]) {
+                dp[i][j] = 1 + dp[i+1][j+1];
+            }
+            else dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+        }
+    }
+    return dp[0][0];
 }
 
 int longestCommonSubsequence(string text1, string text2) {
     dp.clear();
     dp.resize(1005, vector<int>(1005,-1));
-    return f(text1,text2,0,0);
+    // return f(text1,text2,0,0);
+    return f_bu(text1,text2);
 }
 
 int main() {

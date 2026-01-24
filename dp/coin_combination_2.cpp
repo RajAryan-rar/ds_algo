cbb01895b47a057f2f9f52ee20044ba0fa323846
RajAryan-rar <raarra39@gmail.com>
2026-01-24 11:16:40 +0530

    implemented top_down_sol_for_coin_combination_2

--- dp/coin_combination_2.cpp
+++ dp/coin_combination_2.cpp
@@ -1,11 +1,35 @@
 #include<iostream>
 #include<vector>
+#include<cstring>
 using namespace std;
 
 #define mod 1000000007
 
 vector<int> coins;
 vector<int> dp;
+
+/*
+top-down is giving TLE!!!
+To be fixed
+Logic is correct
+
+TLE :-  
+    1. Function Call Overhead
+        In C++, every time you call a function, the computer has to perform several operations:
+        * Pushing arguments onto the stack.
+        * Saving the return address.
+        * Jumping to a new memory location.
+        * Allocating a new stack frame.
+*/
+int fdp[1000005][105];
+int f(int target, int i) {
+    if(target == 0) return 1;
+    if(i >= coins.size() || target < 0) return 0;
+
+    if(fdp[target][i] != -1) return fdp[target][i];
+
+    return fdp[target][i] = (f(target-coins[i], i) + f(target,i+1))%mod;
+}
 
 int f_bu(int target) {
     dp[0] = 1;
@@ -26,8 +50,10 @@ int main() {
     for(int i=0; i<n; i++) {
         cin>>coins[i];
     }
-    dp.resize(target+1,0);
-    cout<<f_bu(target);
+    // dp.resize(target+1,0);
+    // cout<<f_bu(target);
+    memset(fdp, -1, sizeof(fdp));
+    cout<<f(target,0);
 
     return 0;
 }

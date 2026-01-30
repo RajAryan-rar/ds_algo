25d6abc77b641b95b131f8d63019db24625d3f6c
RajAryan-rar <raarra39@gmail.com>
2026-01-30 11:58:30 +0530

    basicqueue_stl

---
+++ queues/basics.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+void reverseQueue(queue<int>& q) {
+    stack<int> st;
+    while(!q.empty()) {
+        st.push(q.front());
+        q.pop();
+    }
+    while(!st.empty()) {
+        q.push(st.top());
+        st.pop();
+    }
+}
+
+void display(queue<int> q) {
+    while (!q.empty()) {
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+    cout<<endl;
+}
+
+int main() {
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+
+    display(q);
+    reverseQueue(q);
+    display(q);
+
+    return 0;
+}

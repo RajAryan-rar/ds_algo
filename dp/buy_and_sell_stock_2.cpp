3dd3e99b9ae4f7328b6e5d5b57da6f6ca32cf71b
RajAryan-rar <raarra39@gmail.com>
2026-02-02 22:10:03 +0530

    solved buy_and_sell_stock_2

---
+++ dp/buy_and_sell_stock_2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+
+        for(int i=1; i<prices.size(); i++) {
+            if(prices[i] - prices[i-1] > 0) ans += prices[i]-prices[i-1];
+        }
+        
+        return ans;
+    }
+};
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    Solution s;
+    cout<<s.maxProfit(arr);
+
+    return 0;
+}

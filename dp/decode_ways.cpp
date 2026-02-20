336be8e14b527b105f75a31a1d29217b47a043f4
RajAryan-rar <raarra39@gmail.com>
2026-02-21 04:48:09 +0530

    solved decode_ways problem

---
+++ dp/decode_ways.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dp;
+    int f(string &s, int i) {
+        if(i < 0) return 1;
+        if(dp[i] != -1) return dp[i];
+
+        int ans = 0;
+
+        if(s[i] != '0') {
+            ans += f(s,i-1);
+        }
+        if(i > 0) {
+            int x = ((s[i-1] - '0') * 10 + (s[i] - '0'));
+            if(x >= 10 && x <= 26) {
+                ans += f(s, i-2);
+            }
+        }
+        return dp[i] = ans;
+    }
+
+    int f2(string &s, int i) {
+        if(i >= s.length()) return 1;
+        if(s[i] == '0') return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int ans = f2(s,i+1);
+        if(i < s.length()-1) {
+            int x = (s[i] - '0')*10 + (s[i+1] - '0');
+            if(x >= 10 && x <= 26) {
+                ans += f2(s,i+2);
+            }
+        }
+        return dp[i] = ans;
+    }
+
+    int numDecodings(string s) {
+        int n = s.length();
+        dp.clear();
+        dp.resize(n+1,-1);
+        return f(s,n-1);
+        // return f2(s,0);
+    }
+};
+
+int main() {
+    string s;
+    cin>>s;
+
+    Solution sol;
+    cout<<sol.numDecodings(s);
+
+    return 0;
+}

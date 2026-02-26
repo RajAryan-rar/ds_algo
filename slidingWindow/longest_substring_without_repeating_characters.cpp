d54e0677f7ec54312fe0ad5f9688f76ac48d65eb
RajAryan-rar <raarra39@gmail.com>
2026-02-26 19:41:18 +0530

    solved longest_substr_without_repeating_characters

---
+++ slidingWindow/longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        vector<int> hash(256,-1);
+
+        int l=0, r=0;
+        int maxLen = 0;
+
+        while(r < n) {
+            if(hash[s[r]] != -1) {
+                if(hash[s[r]] >= l) l = hash[s[r]] + 1;
+            }
+            hash[s[r]] = r;
+            maxLen = max(maxLen, r-l+1);
+            r++;
+        }
+
+        return maxLen;
+    }
+};
+
+int main() {
+    string str;
+    cin>>str;
+
+    Solution s;
+    cout<<s.lengthOfLongestSubstring(str);
+
+
+    return 0;
+}

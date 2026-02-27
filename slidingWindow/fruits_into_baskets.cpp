0b9a13bb31fbb588f889917fbaf274d3dbe5996a
RajAryan-rar <raarra39@gmail.com>
2026-02-27 10:41:08 +0530

    solved fruits_into_basket

---
+++ slidingWindow/fruits_into_baskets.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int bruteForce(vector<int> nums) {
+        int maxLen = 0;
+        
+        for(int i=0; i<nums.size(); i++) {
+            unordered_set<int> st;
+            for(int j=i; j<nums.size(); j++) {
+                st.insert(nums[j]);
+                if(st.size() > 2) break;
+                else maxLen = max(maxLen, j-i+1);
+            }
+        }
+        return maxLen;
+    }
+
+    int totalFruits(vector<int> nums) {
+        unordered_map<int,int> mp;
+        int l = 0, r = 0, maxLen = 0;
+
+        while(r < nums.size()) {
+            mp[nums[r]]++;
+            if(mp.size() <= 2) maxLen = max(maxLen,r-l+1);
+            else {
+                while(mp.size() != 2) {
+                    mp[nums[l]]--;
+                    if(mp[nums[l]] == 0) mp.erase(nums[l]);
+                    l++;
+                }
+            }
+            r++;
+        }
+        return maxLen;
+    }
+};
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    Solution s;
+    cout<<s.totalFruits(arr);
+
+    return 0;
+}

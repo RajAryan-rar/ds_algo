4e614a9e0fc97993a2f817bf826c1cac825ee292
RajAryan-rar <raarra39@gmail.com>
2026-03-03 14:13:55 +0530

    implemented adjacency_list representation for graphs

---
+++ graphs/implementations/adjacency_list.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<list>
+using namespace std;
+
+vector<list<int>> graph;
+
+void add_edge(int src, int dest, bool bi_dir = true) {
+    graph[src].push_back(dest);
+    if(bi_dir) {
+        graph[dest].push_back(src);
+    }
+}
+
+void display() {
+    cout<<endl;
+    for(int i=0; i<graph.size(); i++) {
+        cout<<i<<" --> ";
+        for(auto ele : graph[i]) {
+            cout<<ele<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    int v;
+    cin>>v;
+
+    graph.resize(v, list<int>());
+
+    int e;
+    cin>>e;
+    while(e--) {
+        int s,d;
+        cin>>s>>d;
+        add_edge(s,d);
+    }
+
+    display();
+
+    return 0;
+}
